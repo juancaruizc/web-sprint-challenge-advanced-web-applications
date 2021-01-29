878a0c3e2e9400d5fa86191a9d78962b241f6f0e
juancaruizc <juancruizcruz@gmail.com>
2021-01-29 10:02:55 -0700

    built private route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import PrivateRoute from './components/PrivateRoute';
+import BubblesPage from './components/BubblePage';
 import Login from './components/Login';
 import './styles.scss';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <Router>
       <div className='App'>
+        <PrivateRoute path='/bubbles' component={BubblesPage} />
         <Route exact path='/' component={Login} />
       </div>
     </Router>
