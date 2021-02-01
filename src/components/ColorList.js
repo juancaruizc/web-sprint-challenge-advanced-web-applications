9b17ca37bb84adbf1887c0cb155693de4b2181d2
juancaruizc <juancruizcruz@gmail.com>
2021-02-01 14:42:34 -0700

    delete and put request

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { axiosWithAuth } from '../helpers/axiosWithAuth';
 import EditMenu from './EditMenu';
 import { useParams, useHistory } from 'react-router-dom';
@@ -14,29 +14,49 @@ const ColorList = ({ colors, updateColors }) => {
   const { id } = useParams();
   const { push } = useHistory();
 
+  useEffect(() => {
+    axiosWithAuth()
+      .get('/colors')
+      .then((res) => updateColors(res.data))
+      .catch((err) => console.log(err));
+  }, [updateColors]);
+
   const editColor = (color) => {
     setEditing(true);
     setColorToEdit(color);
   };
-
+  console.log('colors', colors);
   const saveEdit = (e) => {
     e.preventDefault();
     axiosWithAuth()
-      .put(`/colors/${id}`, colorToEdit)
+      .put(`/colors/${colorToEdit.id}`, colorToEdit)
       .then((res) => {
-        updateColors(colors);
+        const updatedColorList = colors.map((color) => {
+          if (color.id === res.data.id) {
+            return res.data;
+          } else {
+            return color;
+          }
+        });
+        updateColors(updatedColorList);
         push('/bubbles');
       })
       .catch((err) => {
         console.log(err);
       });
   };
-
+  console.log(id);
   const deleteColor = (color) => {
+    console.log('second id', color.id);
     axiosWithAuth()
       .delete(`/colors/${color.id}`)
       .then((res) => {
-        push('/bubbles');
+        const deletedColorList = colors.filter(
+          (color) => `${color.id}` != res.data
+        );
+        updateColors(deletedColorList);
+        console.log('res', res.data);
+        // push('/bubbles');
       })
       .catch((err) => console.log(err));
   };
@@ -45,26 +65,28 @@ const ColorList = ({ colors, updateColors }) => {
     <div className='colors-wrap'>
       <p>colors</p>
       <ul>
-        {colors.map((color) => (
-          <li key={color.color} onClick={() => editColor(color)}>
-            <span>
-              <span
-                className='delete'
-                onClick={(e) => {
-                  e.stopPropagation();
-                  deleteColor(color);
-                }}
-              >
-                x
-              </span>{' '}
-              {color.color}
-            </span>
-            <div
-              className='color-box'
-              style={{ backgroundColor: color.code.hex }}
-            />
-          </li>
-        ))}
+        {colors.length
+          ? colors.map((color) => (
+              <li key={color.color} onClick={() => editColor(color)}>
+                <span>
+                  <span
+                    className='delete'
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      deleteColor(color);
+                    }}
+                  >
+                    x
+                  </span>{' '}
+                  {color.color}
+                </span>
+                <div
+                  className='color-box'
+                  style={{ backgroundColor: color.code.hex }}
+                />
+              </li>
+            ))
+          : ''}
       </ul>
       {editing && (
         <EditMenu
